30872cb4d9c4628a2ddb4167c20e86c795bb7a2b
Patrick Dorneles <patrick.o.dorneles@gmail.com>
2021-12-24 22:12:03 -0300

    feat: attempting solution to slow serveless functions connections to DB

--- web/database/index.ts
+++ web/database/index.ts
@@ -2,9 +2,17 @@ import { NODE_ENV } from "../config/Env";
 import mongoose from "mongoose";
 import { MONGOURI } from "../config/Env";
 
+let isDatabaseConnected = false;
+
 export async function connectToDatabase() {
+  if (isDatabaseConnected) {
+    return;
+  }
+
   const databaseName = NODE_ENV;
   mongoose.connect(MONGOURI, {
     dbName: databaseName,
   });
+
+  isDatabaseConnected = true;
 }
